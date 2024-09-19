b2796d5ba1284c751f75354a0e078dde51621f96
Muhammad Saad <143481215+Muhammad-Saad-786@users.noreply.github.com>
2024-09-19 03:28:57 -0700

    Elixir Website
    This is website imported through local storage.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  theme: {
+    extend: {
+      colors: {
+        "midnight-blue": "#2a3855",
+      },
+    },
+  },
+  plugins: [],
+};
